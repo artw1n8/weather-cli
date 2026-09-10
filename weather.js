3876f7033ae8b5cdd28f5460b386952ab1b7ec31
art tem <kalinichenkoartemiywork@gmail.com>
2026-09-10 11:04:30 +0300

    add API token saving

--- weather.js
+++ weather.js
@@ -1,12 +1,25 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { printError, printHelp, printSuccess } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Токен сохранен');
+	} catch (e) {
+		printError(e.message);
+	}
+}
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
 	console.log(args);
 	if(args.h) {
 		printHelp();
+	}
+	if(args.t) {
+		saveToken(args.t);
 	}
 }
 
