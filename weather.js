14907a128189b72ba78e2f5949db770f4c7cad9f
art tem <kalinichenkoartemiywork@gmail.com>
2026-09-10 19:26:03 +0300

    add OpenWeather API integration

--- weather.js
+++ weather.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
 import { printError, printHelp, printSuccess } from "./services/log.service.js";
-import { saveKeyValue } from "./services/storage.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
 
 const saveToken = async (token) => {
+	if(!token.length) {
+		printError('Не передан token');
+		return;
+	}
 	try {
-		await saveKeyValue('token', token);
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
 		printSuccess('Токен сохранен');
 	} catch (e) {
 		printError(e.message);
